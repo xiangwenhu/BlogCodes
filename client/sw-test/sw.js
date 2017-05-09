580fcd11c253d5f2b53ae1bf699f7e03d69205bf
unknown <>
2017-05-09 23:14:21 +0800

    修改cache逻辑

--- client/sw-test/sw.js
+++ client/sw-test/sw.js
@@ -1,6 +1,6 @@
-this.addEventListener('install', function(event) {
+this.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
+    caches.open('v1').then(function (cache) {
       return cache.addAll([
         '/sw-test/',
         '/sw-test/index.html',
@@ -16,17 +16,24 @@ this.addEventListener('install', function(event) {
   );
 });
 
-this.addEventListener('fetch', function(event) {
+this.addEventListener('fetch', function (event) {
   var response;
-  event.respondWith(caches.match(event.request).catch(function() {
-    return fetch(event.request);
-  }).then(function(r) {
-    response = r;
-    caches.open('v1').then(function(cache) {
-      cache.put(event.request, response);
-    });
-    return response.clone();
-  }).catch(function() {
-    return caches.match('/sw-test/gallery/myLittleVader.jpg');
-  }));
+  event.respondWith(caches.match(event.request)
+    .then(r => {
+      return { response: r, cached: true }
+    }).catch(function () {
+      return fetch(event.request).then(r => { return { response: r, cached: false } })
+    }).then(function (r) {
+      let { response, cached } = r
+      if (!cached) {
+        caches.open('v1').then(function (cache) {
+          cache.put(event.request, response);
+        });
+        return response.clone();
+      } else {
+        return response
+      }
+    }).catch(function () {
+      return caches.match('/sw-test/gallery/myLittleVader.jpg');
+    }));
 });

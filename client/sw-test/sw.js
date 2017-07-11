85c4389318f7c7ad1a1f8da27f645be8c766ea4b
胡祥文 <huxiangwen@che001.com>
2017-07-11 11:23:27 +0800

    调试 service worker 版本控制

--- client/sw-test/sw.js
+++ client/sw-test/sw.js
@@ -1,17 +1,21 @@
+const version = 'v4',
+  cacheWhitelist = [version],
+  cacheList = [
+    '/sw-test/',
+    '/sw-test/index.html',
+    '/sw-test/style.css',
+    '/sw-test/app.js',
+    '/sw-test/image-list.js',
+    '/sw-test/star-wars-logo.jpg',
+    '/sw-test/bountyHunters.jpg',
+    '/sw-test/myLittleVader.jpg',
+    '/sw-test/snowTroopers.jpg'
+  ]
+
 this.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open('v1').then(function (cache) {
-      return cache.addAll([
-        '/sw-test/',
-        '/sw-test/index.html',
-        '/sw-test/style.css',
-        '/sw-test/app.js',
-        '/sw-test/image-list.js',
-        '/sw-test/star-wars-logo.jpg',
-        '/sw-test/bountyHunters.jpg',
-        '/sw-test/myLittleVader.jpg',
-        '/sw-test/snowTroopers.jpg'
-      ]);
+    caches.open(version).then(function (cache) {
+      return cache.addAll(cacheList)
     }).then(function () {
       // `skipWaiting()` forces the waiting ServiceWorker to become the
       // active ServiceWorker, triggering the `onactivate` event.
@@ -20,11 +24,11 @@ this.addEventListener('install', function (event) {
       return self.skipWaiting();
     }).catch(err => console.log(err))
   )
-});
+})
 
 this.addEventListener('fetch', function (event) {
 
-  var response;
+
   event.respondWith(caches.match(event.request)
     .then(r => {
       if (r) {
@@ -39,8 +43,8 @@ this.addEventListener('fetch', function (event) {
       return response
     }).catch(function () {
       //return caches.match('/sw-test/gallery/myLittleVader.jpg');
-    }));
-});
+    }))
+})
 
 
 this.addEventListener('message', function (event) {
@@ -58,6 +62,16 @@ this.addEventListener('message', function (event) {
 
 
 self.addEventListener('activate', function (event) {
-  event.waitUntil(self.clients.claim());
-});
+  // event.waitUntil(self.clients.claim())  
+  console.log(version + ' activated')
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(key => {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key)
+        }
+      }))
+    })
+  )
+})
 

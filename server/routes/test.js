1fb63465eb4310c79964339b6de43a3d2f51134f
胡祥文 <huxiangwen@che001.com>
2017-08-23 17:03:27 +0800

    修改测试代码

--- server/routes/test.js
+++ server/routes/test.js
@@ -1,13 +1,23 @@
 const Router = require('koa-router')
-
 var r1 = new Router({
   prefix: '/t'
 })
-
-
-r1.param('id', (id, ctx, next) => {
+r1.get('/test1/:id', function (ctx, next) {
+  console.log('test1 :1')
+  next()
+}, function (ctx, next) {
+  console.log('test1:2')
+})
+r1.get('/test1/:id', function (ctx, next) {
+  console.log('test1 :3')
+  next()
+})
+var r2 = new Router({
+  prefix: '/t2'
+})
+r2.param('id', (id, ctx, next) => {
   if (id > 10) {
-    ctx.status = 404
+      ctx.status = 404
     ctx.body = {
       status: 404
     }
@@ -16,20 +26,5 @@ r1.param('id', (id, ctx, next) => {
   next()
 })
 
-r1.get('/test1/:id', function (ctx, next) {
-  console.log('test1 :1')
-  next()
-}, function (ctx, next) {
-  console.log('test1:2')
-})
-
-
-r1.get('/test1/:id', function (ctx, next) {
-  console.log('test1 :3')
-  next()
-})
-
-
-
-
-module.exports = r1
+r2.use('', r1.routes())
+module.exports = r2
